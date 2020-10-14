298dd2f4501a14937ce02d2bd46fe94eea30fe62
warroyo <warroyo7199008@gmail.com>
2020-10-14 15:34:02 -0600

    initial addition of cert injection

--- env.sh
+++ env.sh
@@ -3,5 +3,6 @@ export DEPLOY_NS="test-ns2"
 export TKC_HTTPS_PROXY="http://someproxy:8080"
 export TKC_HTTP_PROXY="http://someproxy:8080"
 export TKC_NO_PROXY="localhost"
+export REG_CERT="" #the registry ca cert
 #Optional
 #export INTERVAL="30"
--- proxy.sh
+++ proxy.sh
@@ -5,14 +5,30 @@ logerr() { echo "$(date) ERROR: $@" 1>&2; }
 # Logging function that will redirect to stdout with timestamp
 loginfo() { echo "$(date) INFO: $@" ;}
 
-if [[ -z "$TKC_HTTP_PROXY" || -z "$TKC_HTTPS_PROXY" || -z "$TKC_NO_PROXY" ]]
-  then
-    logerr "missing environment vars"
-    exit 2
-fi
+
 
 run_interval=${INTERVAL:=30}
 
+
+function inject_ca()
+{
+  touch /etc/ssl/certs/regcert.pem
+  echo "checking if cert exists"
+  if cmp -s "/etc/ssl/certs/regcert.pem.new" "/etc/ssl/certs/regcert.pem"; then
+      echo "the cert already exists and has not changed"
+  else
+      echo "updating the certs"
+      mv /etc/ssl/certs/regcert.pem.new /etc/ssl/certs/regcert.pem
+      /usr/bin/rehash_ca_certificates.sh
+      systemctl restart containerd
+      echo "certs updated!"
+  fi
+
+}
+
+function insecure_reg(){
+#TODO
+}
 
 function run()
 {
@@ -45,52 +61,68 @@ function run()
       loginfo "attempting ssh to ${ip}"
       ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /tmp/sshkey.pem vmware-system-user@${ip} << EOF
       sudo -i
-      mkdir -p /etc/systemd/system/containerd.service.d
-      touch /etc/systemd/system/containerd.service.d/http-proxy.conf
-      touch /etc/sysconfig/proxy
-
-
-      echo "creating temp containerd proxy file"
-
-      echo '[Service]' > /etc/systemd/system/containerd.service.d/http-proxy.conf.new
-      echo 'Environment="HTTP_PROXY='${TKC_HTTP_PROXY}'"' >> /etc/systemd/system/containerd.service.d/http-proxy.conf.new
-      echo 'Environment="HTTPS_PROXY='${TKC_HTTPS_PROXY}'"' >> /etc/systemd/system/containerd.service.d/http-proxy.conf.new
-      echo 'Environment="NO_PROXY='${TKC_NO_PROXY}'"' >> /etc/systemd/system/containerd.service.d/http-proxy.conf.new
-
-      echo "comparing containerd proxy settings"
-      if cmp -s "/etc/systemd/system/containerd.service.d/http-proxy.conf.new" "/etc/systemd/system/containerd.service.d/http-proxy.conf"; then
-          echo "the containerd proxy is already set and unchanged"
+      if [[ -z "${TKC_HTTP_PROXY}" || -z "${TKC_HTTPS_PROXY}" || -z "${TKC_NO_PROXY}" ]]
+      then
+        echo no PROXY vars set skipping proxy...
       else
-          echo "containerd proxy changed updating..."
-          mv /etc/systemd/system/containerd.service.d/http-proxy.conf.new /etc/systemd/system/containerd.service.d/http-proxy.conf
-          systemctl daemon-reload
-          systemctl restart containerd
+        mkdir -p /etc/systemd/system/containerd.service.d
+        touch /etc/systemd/system/containerd.service.d/http-proxy.conf
+        touch /etc/sysconfig/proxy
+
+
+        echo "creating temp containerd proxy file"
+
+        echo '[Service]' > /etc/systemd/system/containerd.service.d/http-proxy.conf.new
+        echo 'Environment="HTTP_PROXY='${TKC_HTTP_PROXY}'"' >> /etc/systemd/system/containerd.service.d/http-proxy.conf.new
+        echo 'Environment="HTTPS_PROXY='${TKC_HTTPS_PROXY}'"' >> /etc/systemd/system/containerd.service.d/http-proxy.conf.new
+        echo 'Environment="NO_PROXY='${TKC_NO_PROXY}'"' >> /etc/systemd/system/containerd.service.d/http-proxy.conf.new
+
+        echo "comparing containerd proxy settings"
+        if cmp -s "/etc/systemd/system/containerd.service.d/http-proxy.conf.new" "/etc/systemd/system/containerd.service.d/http-proxy.conf"; then
+            echo "the containerd proxy is already set and unchanged"
+        else
+            echo "containerd proxy changed updating..."
+            mv /etc/systemd/system/containerd.service.d/http-proxy.conf.new /etc/systemd/system/containerd.service.d/http-proxy.conf
+            systemctl daemon-reload
+            systemctl restart containerd
+        fi
+
+
+        echo "creating temp system proxy file"
+
+        echo 'PROXY_ENABLED="yes"' > /etc/sysconfig/proxy.new
+        echo 'HTTP_PROXY='"'${TKC_HTTP_PROXY}'" >> /etc/sysconfig/proxy.new
+        echo 'HTTPS_PROXY='"'${TKC_HTTPS_PROXY}'" >> /etc/sysconfig/proxy.new
+        echo 'NO_PROXY='"'${TKC_NO_PROXY}'" >> /etc/sysconfig/proxy.new
+
+        echo "comparing system proxy settings"
+        if cmp -s "/etc/sysconfig/proxy" "/etc/sysconfig/proxy.new"; then
+            echo "the system proxy is already set and unchanged"
+        else
+            echo "system proxy changed updating..."
+            mv /etc/sysconfig/proxy.new /etc/sysconfig/proxy
+        fi
       fi
 
-
-      echo "creating temp system proxy file"
-
-      echo 'PROXY_ENABLED="yes"' > /etc/sysconfig/proxy.new
-      echo 'HTTP_PROXY='"'${TKC_HTTP_PROXY}'" >> /etc/sysconfig/proxy.new
-      echo 'HTTPS_PROXY='"'${TKC_HTTPS_PROXY}'" >> /etc/sysconfig/proxy.new
-      echo 'NO_PROXY='"'${TKC_NO_PROXY}'" >> /etc/sysconfig/proxy.new
-
-      echo "comparing system proxy settings"
-      if cmp -s "/etc/sysconfig/proxy" "/etc/sysconfig/proxy.new"; then
-          echo "the system proxy is already set and unchanged"
+      if [[ -z "${REG_CERT}" ]]
+      then
+        echo no CA cert providied skipping cert injection...
       else
-          echo "system proxy changed updating..."
-          mv /etc/sysconfig/proxy.new /etc/sysconfig/proxy
+       $(typeset -f inject_ca)
+       inject_ca
       fi
+
 
 EOF
 
   if [ $? -eq 0 ] ;
   then  
-        loginfo "Proxy added successfully!"
+        loginfo "script ran successfully!"
   else
-        logerr "There was an error writing the proxy to /etc/systemd/system/containerd.service.d/http-proxy.conf Exiting..."
+        logerr "There was an error running the script Exiting..."
   fi
+
+
 loginfo "-------------------"
   done
 }
